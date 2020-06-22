ab2afd61eb3ffa9a7ac7e7dc863d750f5165dcdc
anshulameta47 <anshulameta@gmail.com>
2020-06-22 23:11:56 +0530

    Java-project added

---
+++ java-project/src/main/java/com/sapient/CheckEven.java
@@ -0,0 +1,16 @@
+package com.sapient;
+
+/**
+ * Hello world!
+ *
+ */
+public class CheckEven 
+{
+    public int Check(int n)
+    {
+    	if(n%2==0)
+    		return 1;
+    	else
+    		return 0;
+    }
+}
